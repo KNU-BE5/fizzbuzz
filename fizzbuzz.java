dd4634447fd182888a61d7d13e16a939fcd0dc01
eunsoni <esk12704@gmail.com>
2024-06-04 20:51:38 +0900

    resolve #4

--- fizzbuzz.java
+++ fizzbuzz.java
@@ -1,3 +1,8 @@
 public class fizzbuzz{
-
+    public static boolean fizzBuzz(int number) {
+        if (number % 3 != 0 && number % 5 != 0) {
+           return true;
+        }
+        return false;
+    }
 }
